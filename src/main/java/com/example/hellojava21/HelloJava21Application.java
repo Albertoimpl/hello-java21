5cb7097cc2dc2ca54d7a88c6c3aa70610e3a86a9
Alberto C. Ríos <albertoimpl@gmail.com>
2024-01-29 14:07:58 +0100

    First commit

---
+++ src/main/java/com/example/hellojava21/HelloJava21Application.java
@@ -0,0 +1,13 @@
+package com.example.hellojava21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJava21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloJava21Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hellojava21/HelloJava21ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.hellojava21;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJava21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
